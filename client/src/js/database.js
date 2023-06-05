6b8777a46aef751b25c7c8408ea09625cf3ff97a
MOX UFO <mox.ufo@gmail.com>
2023-06-05 18:21:59 -0400

    webpack config and database files are completed

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,62 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const postDb = async (content) => {
+  try {
+    try {
+      const db = await openDB('jate', 1);
+      const tx = db.transaction('jate', 'readwrite');
+      const store = tx.objectStore('jate');
+      const note = store.add(content);
+      // await tx.done;
+      console.log('Content added to the database:', note);
+    } catch (error) {
+      console.log('Error adding content to the database:', error);
+    }
+  } catch (error) {
+    console.log("PUT DB ERROR")
+    console.log(error)
+  }
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    console.log('GETTNG DB')
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const allContent = await store.getAll();
+    console.log('All content from the database:', allContent);
+    return allContent;
+  } catch (error) {
+    console.log('ERROR WITH DB')
+    console.log(error)
+  }
+}
+
+export const deleteDb = async (id) => {
+  console.log('DELETE from the database', id);
+
+
+  const contactDb = await openDB('jate', 1);
+
+
+  const tx = contactDb.transaction('jate', 'readwrite');
+
+
+  const store = tx.objectStore('jate');
+
+
+  const request = store.delete(id);
+
+
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+};
 
 initdb();
+
+
+ 
